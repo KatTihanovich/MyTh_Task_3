0632d21ebc72013723d0591ddc468e0203f8e95c
KatTihanovich <kataviridi@gmail.com>
2025-01-30 10:51:52 +0200

    Fixed path

--- src/main/java/com/ferry/main/Main.java
+++ src/main/java/com/ferry/main/Main.java
@@ -5,9 +5,10 @@ import com.ferry.manager.FerryManager;
 import java.util.concurrent.TimeUnit;
 
 public class Main {
+    private static final String FILE_PATH = "/cars.txt";
     public static void main(String[] args) {
         FerryManager ferryManager = FerryManager.getInstance();
-        ferryManager.startSimulation("/cars.txt");
+        ferryManager.startSimulation(FILE_PATH);
         try {
             TimeUnit.SECONDS.sleep(45);
         } catch (InterruptedException e) {
